38f2798a76291e01af1d7857fa9846832a5ddf01
TonioCoding <ToneBone7960@gmail.com>
2023-11-08 20:48:44 -0500

    curretn login page progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import{BrowserRouter, Routes, Route} from "react-router-dom";
+import {LoginPage} from "./Routes.js";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
