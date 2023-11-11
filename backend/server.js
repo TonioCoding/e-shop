9a78bda97f9786d4825303b6e3810647adfe0bda
TonioCoding <ToneBone7960@gmail.com>
2023-11-10 21:19:10 -0500

    fixed typo

--- backend/server.js
+++ backend/server.js
@@ -19,15 +19,13 @@ connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 //unhandle promise rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Shutting down the sever for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
+  console.log(`Shutting down the server for unhandle promise rejection`);
 
   server.close(() => {
     process.exit(1);
